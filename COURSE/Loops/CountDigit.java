4acdaccac6366fb079a31a71582cce381005cda2
akashrwt8 <akashsinghrawat80@gmail.com>
2024-05-23 14:19:03 +0530

    Adding code for counting digit of a number

---
+++ COURSE/Loops/CountDigit.java
@@ -0,0 +1,24 @@
+package COURSE.Loops;
+
+import java.util.Scanner;
+
+public class CountDigit {
+
+    public static int count(int n) {
+        int c = 0;
+        while (n != 0) {
+            int d = n % 10;
+            n /= 10;
+            c++;
+        }
+        return c;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number to count it's digit : ");
+        int n = sc.nextInt();
+        System.out.print("Digit count is " + count(n));
+        sc.close();
+    }
+}
+
